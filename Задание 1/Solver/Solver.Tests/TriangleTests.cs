aa564f1704d69a75f01311ab331c06cf632bcd9f
defau1tusername <a-chapay@mail.ru>
2023-04-24 19:11:10 +0300

    ...

--- Задание 1/Solver/Solver.Tests/TriangleTests.cs
+++ Задание 1/Solver/Solver.Tests/TriangleTests.cs
@@ -19,8 +19,8 @@ public class TriangleTests
     [TestCase(3, 4, 5, true)]
     [TestCase(4, 16, 15.491933384829668, true)]
     [TestCase(2, 2, 3, false)]
-    public void IsRight(double a, double b, double c, bool isRight)
+    public void IsRight(double a, double b, double c, bool expected)
     {
-        Assert.AreEqual(isRight, new Triangle(a, b, c).IsRight());
+        Assert.AreEqual(expected, new Triangle(a, b, c).IsRight());
     }
 }

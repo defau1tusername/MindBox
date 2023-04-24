6bb7e3e3cb9689df8056d0fdb74cccef6134ffea
defau1tusername <a-chapay@mail.ru>
2023-04-24 18:55:16 +0300

    ....

--- Задание 1/Solver/Solver.Tests/TriangleTests.cs
+++ Задание 1/Solver/Solver.Tests/TriangleTests.cs
@@ -15,4 +15,12 @@ public class TriangleTests
     {
         Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
     }
+
+    [TestCase(3, 4, 5, true)]
+    [TestCase(4, 16, 15.491933384829668, true)]
+    [TestCase(2, 2, 3, false)]
+    public void IsRight(double a, double b, double c, bool isRight)
+    {
+        Assert.AreEqual(isRight, new Triangle(a, b, c).IsRight());
+    }
 }

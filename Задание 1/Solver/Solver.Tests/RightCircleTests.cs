4954ffa59c845f1c1d4d45c7f7bf137d180784c9
defau1tusername <a-chapay@mail.ru>
2023-04-24 18:50:55 +0300

    ...

---
+++ Задание 1/Solver/Solver.Tests/RightCircleTests.cs
@@ -0,0 +1,18 @@
+﻿[TestFixture]
+public class TriangleTests
+{
+    [TestCase(1, 1, 1, 0.4330127018922193)]
+    [TestCase(2, 3, 4, 2.9047375096555625)]
+    [TestCase(12, 2, 12, 11.958260743101398)]
+    public void AreaCalculation_IsCorrect(double a, double b, double c, double expected)
+    {
+        Assert.AreEqual(expected, new Triangle(a, b, c).Area);
+    }
+
+    [TestCase(0, 0, 0)]
+    [TestCase(-12, -2, -12)]
+    public void Constructor_WhenRadiusNonPositiveOrZero_ShouldThrowException(double a, double b, double c)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
+    }
+}
--- Задание 1/Solver/Solver.Tests/TriangleTests.cs
+++ Задание 1/Solver/Solver.Tests/TriangleTests.cs
@@ -1,18 +1,11 @@
 ﻿[TestFixture]
-public class TriangleTests
+public class RightCircleTests
 {
-    [TestCase(1, 1, 1, 0.4330127018922193)]
-    [TestCase(2, 3, 4, 2.9047375096555625)]
-    [TestCase(12, 2, 12, 11.958260743101398)]
-    public void AreaCalculation_IsCorrect(double a, double b, double c, double expected)
+    [TestCase(3, 4, 5, true)]
+    [TestCase(4, 16, 15.491933384829668, true)]
+    [TestCase(2, 2, 3, false)]
+    public void IsRight(double a, double b, double c, bool isRight)
     {
-        Assert.AreEqual(expected, new Triangle(a, b, c).Area);
-    }
-
-    [TestCase(0, 0, 0)]
-    [TestCase(-12, -2, -12)]
-    public void Constructor_WhenRadiusNonPositiveOrZero_ShouldThrowException(double a, double b, double c)
-    {
-        Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
+        Assert.AreEqual(isRight, new Triangle(a, b, c).IsRight());
     }
 }

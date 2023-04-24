31d45a1934c54d0c1675c3d2d4e3647fb52bf923
defau1tusername <a-chapay@mail.ru>
2023-04-24 17:30:50 +0300

    ...

---
+++ Задание 1/Solver/Solver.Tests/CircleTests.cs
@@ -0,0 +1,20 @@
+
+[TestFixture]
+public class CircleTests
+{
+    [TestCase(0,0)]
+    [TestCase(3.2, 32.169908772759484)]
+    [TestCase(39.0, 4778.362426110075)]
+    [TestCase(23478324, 1731745452388171.5)]
+    public void AreaCalculation_IsCorrect(double radius, double expected)
+    {
+        Assert.AreEqual(expected, new Circle(radius).Area);
+    }
+
+    [TestCase(-10)]
+    [TestCase(-12391012)]
+    public void Constructor_WhenRadiusNonPositive_ShouldThrowException(double radius)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(radius));
+    }
+}
